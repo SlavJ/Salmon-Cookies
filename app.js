7a522f8b74d24692cd455afa52f64e68ae0985d6
Danny <danny.mdj07@gmail.com>
2023-06-21 16:55:12 +0100

    table

--- app.js
+++ app.js
@@ -6,25 +6,59 @@ const container = document.getElementById("container");
 //defining the open hours
 let hours = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
+let headerHours = [
+  "",
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  "Daily Location Total",
+];
+
 // code for statistics
-let seattle = {
+const seattle = {
   storeName: "Seattle",
   minCustPerHour: 23,
   maxCustPerHour: 65,
   avgCookiesPerCust: 6.3,
-  customersEachHour: function () {
-    return Math.floor(
-      Math.random() * (this.maxCustPerHour - this.minCustPerHour) +
-        this.minCustPerHour
-    );
+  customersEachHour: [],
+  cookiesEachHour: [],
+  totalDailyCookies: 0,
+  calcCustomersEachHour: function () {
+    for (let i = 0; i < hours.length; i++) {
+      this.customersEachHour.push(
+        Math.floor(
+          Math.random() * (this.maxCustPerHour - this.minCustPerHour) +
+            this.minCustPerHour
+        )
+      );
+    }
   },
-  cookiesEachHour: function () {
-    return this.customersEachHour * this.avgCookiesPerCust;
+  calcCookiesEachHour: function () {
+    for (let i = 0; i < hours.length; i++) {
+      const oneHour = this.cookiesEachHour.push(
+        Math.ceil(this.customersEachHour[i] * this.avgCookiesPerCust)
+      );
+      this.totalDailyCookies += oneHour;
+    }
   },
-  totalDailyCookies: function () {
+  totalOfDailyCookies: function () {
     return this.cookiesEachHour * hours.length;
   },
 };
+seattle.calcCustomersEachHour();
+seattle.calcCookiesEachHour();
+seattle.totalOfDailyCookies();
 
 // parent and child tags
 let article = document.createElement("article");
@@ -37,44 +71,25 @@ article.appendChild(h3);
 let ul = document.createElement("ul");
 article.appendChild(ul);
 
-// generates random number for list
-for (let i = 0; i < hours.length; i++) {
-  const li = document.createElement("li");
-  li.textContent = `${hours(i)}: ${this.cookiesEachHour(i)} cookies`;
-  appendChild(i);
-}
-
-// table display
-let table = document.createElement("table");
-article.appendChild(table);
-
 //  the header row
-let headerRow = document.createElement("tr");
-table.appendChild(headerRow);
 
-// table header cells
-let seattleHeaderCell = document.createElement("th");
-seattleHeaderCell.textContent = "Seattle";
-headerRow.appendChild(seattleHeaderCell);
+function renderTable() {
+  let table = document.getElementById("my-table");
+  let headerRow = document.createElement("tr");
+  for (let i = 0; i < headerHours.length; i++) {
+    let headerCell = document.createElement("th");
+    headerCell.textContent = headerHours[i];
+    headerRow.appendChild(headerCell);
+  }
+  table.appendChild(headerRow);
 
-let tokyoHeaderCell = document.createElement("th");
-tokyoHeaderCell.textContent = "Tokyo";
-headerRow.appendChild(tokyoHeaderCell);
-
-let dubaiHeaderCell = document.createElement("th");
-dubaiHeaderCell.textContent = "Dubai";
-headerRow.appendChild(dubaiHeaderCell);
-
-let parisHeaderCell = document.createElement("th");
-parisHeaderCell.textContent = "Paris";
-headerRow.appendChild(parisHeaderCell);
-
-let limaHeaderCell = document.createElement("th");
-limaHeaderCell.textContent = "Lima";
-headerRow.appendChild(limaHeaderCell);
-
-// data row
-let dataRow = document.createElement("tr");
-table.appendChild(dataRow);
+  let tableRow = document.createElement("tr");
+  for (let i = 0; i < headerHours.length; i++) {
+    let headerCell = document.createElement("td");
+    tableRow.appendChild(headerCell);
+  }
+  table.appendChild(tableRow);
+}
+renderTable();
 
 // data cell
